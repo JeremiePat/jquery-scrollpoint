e4a9ba87489e821a9d071111ce0aa8fead818b94
Jeremie Patonnier <jeremie.patonnier@gmail.com>
2012-05-19 00:57:20 +0200

    Adding licence

--- jQuery.scrollPoint.js
+++ jQuery.scrollPoint.js
@@ -1,46 +1,59 @@
-/**
- * The scrollPoint plugin allow to define scrolling zones to interact with them
- *
- * The plugin defines 3 custom events :
- *  - scrollPointEnter
- *  - scrollPointLeave
- *  - scrollPointMove
- */
+/*
+    Copyright (c) 2012
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
 (function($){
-	$.fn.scrollPoint = function( params ) {
-		params = $.extend({
-			up: 0, 
-			down: 0
-		}, params);
-		
-		return this.each(function() {
-			var element = $(this),
-				$window = $(window),
-				isIn = false;
-			    hasStarted = false;
-	
-			$window.scroll(function() {
-				var Event = $.Event("scrollPointMove"), 
-					pos = $window.scrollTop(),
-				    oldIn = isIn;
-				
-				isIn = pos >= params.up && pos <= params.down;
-				
-				if(oldIn !== isIn) {
-					if(!hasStarted && isIn) {
-						hasStarted = true;
-						element.trigger("scrollPointEnter");
-					}
-				
-					if (hasStarted && !isIn) {
-						hasStarted = false;
-						element.trigger("scrollPointLeave");
-					}
-				}
-				
-				Event.isIn = isIn;
-				element.trigger(Event);
-			});
-		});		  
-	};
+    $.fn.scrollPoint = function( params ) {
+        params = $.extend({
+            up: 0,
+            down: 0
+        }, params);
+        
+        return this.each(function() {
+            var element = $(this),
+                $window = $(window),
+                isIn = false;
+                hasStarted = false;
+    
+            $window.scroll(function() {
+                var Event = $.Event("scrollPointMove"),
+                    pos = $window.scrollTop(),
+                    oldIn = isIn;
+                
+                isIn = pos >= params.up && pos <= params.down;
+                
+                if(oldIn !== isIn) {
+                    if(!hasStarted && isIn) {
+                        hasStarted = true;
+                        element.trigger("scrollPointEnter");
+                    }
+                
+                    if (hasStarted && !isIn) {
+                        hasStarted = false;
+                        element.trigger("scrollPointLeave");
+                    }
+                }
+                
+                Event.isIn = isIn;
+                element.trigger(Event);
+            });
+        });
+    };
 })(jQuery);

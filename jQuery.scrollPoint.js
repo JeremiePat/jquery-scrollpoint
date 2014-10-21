974b088cbf02337ffe480ee00276388d1dc9ae58
Thomas ZILLIOX <thomas@zilliox.me>
2014-10-21 10:38:19 +0200

    Update copyright dates

--- jQuery.scrollPoint.js
+++ jQuery.scrollPoint.js
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2014 Jeremie Patonnier
+    Copyright (c) 2012 - 2014 Jeremie Patonnier
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
